be7c763670b836b93930311d99dd5909274db5c6
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-20 13:14:11 +0530

    Create App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import { Component } from 'react'
+import UserCreate from './UserCreate'
+import { LanguageStore } from '../contexts/LanguageContext'
+import LanguageSelector from './LanguageSelector'
+import ColorContext from '../contexts/ColorContext'
+class App extends Component {
+  // state=  {
+  //         language : 'english',
+  //         };
+  // onLanguageChange = ( language ) => {
+  //     this.setState({ language });
+  // }
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="black">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    )
+  }
+}
+export default App
