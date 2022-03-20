4066cdc8c0ee55915ddaabb606e85a09dd828e1a
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-20 13:49:23 +0530

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+const rootElement = document.querySelector('#root')! as HTMLDivElement //  rootElement
+ReactDOM.render(<App />, rootElement)
